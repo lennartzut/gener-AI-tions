841f106abbda7cf852e0c106df702cccecbdb6e1
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2025-01-06 10:53:46 +0100

    Adds dynamical relationship between siblings

--- app/schemas/identity_schema.py
+++ app/schemas/identity_schema.py
@@ -1,6 +1,8 @@
 from datetime import date, datetime
 from typing import Optional
+
 from pydantic import BaseModel, Field, model_validator
+
 from app.models.enums_model import GenderEnum
 from app.utils.validators_utils import ValidationUtils
 
@@ -9,14 +11,22 @@ class IdentityBase(BaseModel):
     """
     Base schema for an identity, including common fields.
     """
-    first_name: Optional[str] = Field(None, min_length=1, max_length=50, description="First name of the individual")
-    last_name: Optional[str] = Field(None, min_length=1, max_length=50, description="Last name of the individual")
-    gender: Optional[GenderEnum] = Field(None, description="Gender of the individual")
-    valid_from: Optional[date] = Field(None, description="The start date of this identity's validity")
-    valid_until: Optional[date] = Field(None, description="The end date of this identity's validity")
+    first_name: Optional[str] = Field(None, min_length=1,
+                                      max_length=50,
+                                      description="First name of the individual")
+    last_name: Optional[str] = Field(None, min_length=1,
+                                     max_length=50,
+                                     description="Last name of the individual")
+    gender: Optional[GenderEnum] = Field(None,
+                                         description="Gender of the individual")
+    valid_from: Optional[date] = Field(None,
+                                       description="The start date of this identity's validity")
+    valid_until: Optional[date] = Field(None,
+                                        description="The end date of this identity's validity")
 
     @model_validator(mode='after')
-    def validate_dates(cls, values: 'IdentityBase') -> 'IdentityBase':
+    def validate_dates(cls,
+                       values: 'IdentityBase') -> 'IdentityBase':
         """
         Validates that `valid_from` is earlier than `valid_until`.
         """
@@ -36,22 +46,32 @@ class IdentityCreate(IdentityBase):
     """
     Schema for creating a new identity.
     """
-    individual_id: int = Field(..., description="ID of the associated individual")
+    individual_id: int = Field(...,
+                               description="ID of the associated individual")
 
 
 class IdentityUpdate(BaseModel):
     """
     Schema for updating an existing identity.
     """
-    first_name: Optional[str] = Field(None, min_length=1, max_length=50, description="Updated first name")
-    last_name: Optional[str] = Field(None, min_length=1, max_length=50, description="Updated last name")
-    gender: Optional[GenderEnum] = Field(None, description="Updated gender")
-    valid_from: Optional[date] = Field(None, description="Updated start date of validity")
-    valid_until: Optional[date] = Field(None, description="Updated end date of validity")
-    is_primary: Optional[bool] = Field(None, description="Whether this identity is the primary identity")
+    first_name: Optional[str] = Field(None, min_length=1,
+                                      max_length=50,
+                                      description="Updated first name")
+    last_name: Optional[str] = Field(None, min_length=1,
+                                     max_length=50,
+                                     description="Updated last name")
+    gender: Optional[GenderEnum] = Field(None,
+                                         description="Updated gender")
+    valid_from: Optional[date] = Field(None,
+                                       description="Updated start date of validity")
+    valid_until: Optional[date] = Field(None,
+                                        description="Updated end date of validity")
+    is_primary: Optional[bool] = Field(None,
+                                       description="Whether this identity is the primary identity")
 
     @model_validator(mode='after')
-    def validate_dates(cls, values: 'IdentityUpdate') -> 'IdentityUpdate':
+    def validate_dates(cls,
+                       values: 'IdentityUpdate') -> 'IdentityUpdate':
         """
         Validates that `valid_from` is earlier than `valid_until`.
         """
@@ -72,10 +92,14 @@ class IdentityOut(IdentityBase):
     Schema for returning identity data.
     """
     id: int = Field(..., description="The unique ID of the identity")
-    individual_id: int = Field(..., description="The ID of the associated individual")
-    is_primary: bool = Field(..., description="Indicates if this is the primary identity")
-    created_at: datetime = Field(..., description="The timestamp when this identity was created")
-    updated_at: datetime = Field(..., description="The timestamp when this identity was last updated")
+    individual_id: int = Field(...,
+                               description="The ID of the associated individual")
+    is_primary: bool = Field(...,
+                             description="Indicates if this is the primary identity")
+    created_at: datetime = Field(...,
+                                 description="The timestamp when this identity was created")
+    updated_at: datetime = Field(...,
+                                 description="The timestamp when this identity was last updated")
 
     class Config:
         from_attributes = True
--- app/schemas/individual_schema.py
+++ app/schemas/individual_schema.py
@@ -1,22 +1,32 @@
 from datetime import date
 from typing import Optional, List
-from pydantic import (BaseModel, Field, model_validator)
+
+from pydantic import BaseModel, Field, model_validator
+
 from app.models.enums_model import GenderEnum
 from app.schemas.identity_schema import IdentityOut
 from app.utils.validators_utils import ValidationUtils
 
 
 class IndividualBase(BaseModel):
-    birth_date: Optional[date] = Field(None, description="Birth date of the individual")
-    birth_place: Optional[str] = Field(None, max_length=100, description="Birthplace of the individual")
-    death_date: Optional[date] = Field(None, description="Death date of the individual")
-    death_place: Optional[str] = Field(None, max_length=100, description="Place of death for the individual")
-    notes: Optional[str] = Field(None, description="Additional notes about the individual")
+    birth_date: Optional[date] = Field(None,
+                                       description="Birth date of the individual")
+    birth_place: Optional[str] = Field(None, max_length=100,
+                                       description="Birthplace of the individual")
+    death_date: Optional[date] = Field(None,
+                                       description="Death date of the individual")
+    death_place: Optional[str] = Field(None, max_length=100,
+                                       description="Place of death for the individual")
+    notes: Optional[str] = Field(None,
+                                 description="Additional notes about the individual")
 
     @model_validator(mode='after')
-    def validate_date_order(cls, values: 'IndividualBase') -> 'IndividualBase':
+    def validate_date_order(cls,
+                            values: 'IndividualBase') -> 'IndividualBase':
         ValidationUtils.validate_date_order(
-            values.birth_date, values.death_date, "Birth date must be before death date.")
+            values.birth_date, values.death_date,
+            "Birth date must be before death date."
+        )
         return values
 
     class Config:
@@ -24,45 +34,71 @@ class IndividualBase(BaseModel):
 
 
 class IndividualCreate(IndividualBase):
-    first_name: str = Field(..., min_length=1, max_length=50, description="First name of the individual")
-    last_name: str = Field(..., min_length=1, max_length=50, description="Last name of the individual")
-    gender: GenderEnum = Field(..., description="Gender of the individual")
+    first_name: str = Field(..., min_length=1, max_length=50,
+                            description="First name of the individual")
+    last_name: str = Field(..., min_length=1, max_length=50,
+                           description="Last name of the individual")
+    gender: GenderEnum = Field(...,
+                               description="Gender of the individual")
 
 
 class IndividualUpdate(IndividualBase):
-    first_name: Optional[str] = Field(None, min_length=1, max_length=50, description="Updated first name")
-    last_name: Optional[str] = Field(None, min_length=1, max_length=50, description="Updated last name")
-    gender: Optional[GenderEnum] = Field(None, description="Updated gender")
+    first_name: Optional[str] = Field(None, min_length=1,
+                                      max_length=50,
+                                      description="Updated first name")
+    last_name: Optional[str] = Field(None, min_length=1,
+                                     max_length=50,
+                                     description="Updated last name")
+    gender: Optional[GenderEnum] = Field(None,
+                                         description="Updated gender")
 
 
 class IndividualOut(BaseModel):
     class RelatedIndividualOut(BaseModel):
         id: int = Field(...,
                         description="The unique ID of the related individual")
-        first_name: Optional[str] = Field(None, description="First name of the related individual")
-        last_name: Optional[str] = Field(None, description="Last name of the related individual")
+        first_name: Optional[str] = Field(None,
+                                          description="First name of the related individual")
+        last_name: Optional[str] = Field(None,
+                                         description="Last name of the related individual")
+        relationship_id: Optional[int] = Field(None,
+                                               description="The ID of the relationship between the individuals")
 
         class Config:
             from_attributes = True
 
-    id: int = Field(..., description="The unique ID of the individual")
-    number: int = Field(..., description="A unique number assigned to the individual")
-    birth_date: Optional[date] = Field(None, description="Birth date of the individual")
-    birth_place: Optional[str] = Field(None, description="Birthplace of the individual")
-    death_date: Optional[date] = Field(None, description="Death date of the individual")
-    death_place: Optional[str] = Field(None, description="Place of death for the individual")
-    notes: Optional[str] = Field(None, description="Additional notes about the individual")
-    age: Optional[int] = Field(None, description="Age of the individual, calculated if applicable")
-    primary_identity: Optional[IdentityOut] = Field(None, description="The primary identity associated with the individual")
-    identities: List[IdentityOut] = Field(default_factory=list, description="All identities associated with the individual")
+    id: int = Field(...,
+                    description="The unique ID of the individual")
+    number: int = Field(...,
+                        description="A unique number assigned to the individual")
+    birth_date: Optional[date] = Field(None,
+                                       description="Birth date of the individual")
+    birth_place: Optional[str] = Field(None,
+                                       description="Birthplace of the individual")
+    death_date: Optional[date] = Field(None,
+                                       description="Death date of the individual")
+    death_place: Optional[str] = Field(None,
+                                       description="Place of death for the individual")
+    notes: Optional[str] = Field(None,
+                                 description="Additional notes about the individual")
+    age: Optional[int] = Field(None,
+                               description="Age of the individual, calculated if applicable")
+    primary_identity: Optional[IdentityOut] = Field(None,
+                                                    description="The primary identity associated with the individual")
+    identities: List[IdentityOut] = Field(default_factory=list,
+                                          description="All identities associated with the individual")
     partners: List['IndividualOut.RelatedIndividualOut'] = Field(
-        default_factory=list, description="List of partner details")
+        default_factory=list, description="List of partner details"
+    )
     parents: List['IndividualOut.RelatedIndividualOut'] = Field(
-        default_factory=list, description="List of parent details")
+        default_factory=list, description="List of parent details"
+    )
     children: List['IndividualOut.RelatedIndividualOut'] = Field(
-        default_factory=list, description="List of child details")
+        default_factory=list, description="List of child details"
+    )
     siblings: List['IndividualOut.RelatedIndividualOut'] = Field(
-        default_factory=list, description="List of sibling details")
+        default_factory=list, description="List of sibling details"
+    )
 
     @model_validator(mode='after')
     def populate_age(cls,
