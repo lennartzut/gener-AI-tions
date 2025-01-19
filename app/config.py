d65943ee6672404313693288036c67972881abb2
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2025-01-19 17:55:29 +0100

    Changes token expire time

--- app/config.py
+++ app/config.py
@@ -22,7 +22,7 @@ class Config:
     JWT_SECRET_KEY = os.getenv(
         'JWT_SECRET_KEY') or secrets.token_urlsafe(32)
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(
-        seconds=int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 3600)))
+        seconds=int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 7200)))
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(
         seconds=int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES', 604800)))
     JWT_COOKIE_SECURE = os.getenv('JWT_COOKIE_SECURE',
